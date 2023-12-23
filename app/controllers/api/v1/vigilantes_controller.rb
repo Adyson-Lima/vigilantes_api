5075e472338baa87fc81f0b742e2a5b44b5e6910
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-23 16:25:52 -0300

    23° commit - tdd controller: Consegue criar um vigilante e retornar status 201? GREEN

--- app/controllers/api/v1/vigilantes_controller.rb
+++ app/controllers/api/v1/vigilantes_controller.rb
@@ -11,6 +11,15 @@ class Api::V1::VigilantesController < ApplicationController
     render json: @vigilante
   end
 
+  def create
+    @vigilante = Vigilante.new(vigilante_params)
+    if @vigilante.save 
+      render json: @vigilante, status: :created, location: api_v1_vigilante_url(@vigilante)
+    else
+      render json: @vigilante.errors, status: :unprocessable_entity
+    end
+  end
+
 private
 
 def set_vigilante
