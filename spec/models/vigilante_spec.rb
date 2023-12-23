1f2355052ed81feb29e8a2e52bb94f8b33f98209
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-23 15:36:11 -0300

    10° commit - tdd model: name consegue ser preenchido?  RED

---
+++ spec/models/vigilante_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Vigilante, type: :model do
+
+  before{@vigilante = Vigilante.new}
+
+  describe 'testes de preenchimento do model Vigilante' do
+
+    it 'name consegue ser preenchido?' do
+      @vigilante.name = ''
+      expect(@vigilante.name).to eq('dave')
+    end
+
+  end
+
+end
