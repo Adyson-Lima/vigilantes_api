ebe5c8af78a594f58d00ead79ef8872bc5036169
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-23 16:12:28 -0300

    20° commit - tdd controller: Consegue listar um vigilante especifico e retornar tstaus 200? RED

--- spec/controllers/vigilantes_controller_v1_spec.rb
+++ spec/controllers/vigilantes_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::VigilantesController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/vigilantes/id' do
+    it 'Consegue listar um vigilante especifico e retornar tstaus 200?' do
+      get :show, params: {id: @vigilante.id}
+      expect(response.body).to include_json(name: 'J. Torque')
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
