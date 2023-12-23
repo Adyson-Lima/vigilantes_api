be5f77f0794eae7162797d381256f3908588e0a3
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-23 15:30:41 -0300

    9° commit - tdd folders: a pasta app/controllers/api/v1 existe? GREEN

--- spec/folders_spec.rb
+++ spec/folders_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Testes de verificação de existencia de pastas' do
     expect(Dir.exist?('app/controllers/api')).to eq(true)
   end
 
-  it 'a pasta app/controllers/api/v1 esiste?' do
+  it 'a pasta app/controllers/api/v1 existe?' do
     expect(Dir.exist?('app/controllers/api/v1')).to eq(true)
   end
 
